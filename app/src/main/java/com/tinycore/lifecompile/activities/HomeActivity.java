6ebdce6eef47c8def82b29835cc8161d9366128b
Lee McKinnon <leemac314@gmail.com>
2016-05-13 08:05:03 -0400

    drawer working, messy, but working

--- app/src/main/java/com/tinycore/lifecompile/activities/HomeActivity.java
+++ app/src/main/java/com/tinycore/lifecompile/activities/HomeActivity.java
@@ -4,15 +4,27 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.tinycore.lifecompile.R;
 import com.tinycore.lifecompile.fragments.NoteListFragment;
 
+import java.util.ArrayList;
+
 public class HomeActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ArrayList<String> mMenuItems;
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +36,61 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        mMenuItems = new ArrayList<>();
+
+        mMenuItems.add("Home");
+        mMenuItems.add("Tags");
+        mMenuItems.add("Sign Out");
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mMenuItems));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,        /* DrawerLayout object */
+                mToolbar,  /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle("The title");
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("The drawer title");
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
         Bundle bundle = this.getIntent().getExtras();
 
-        if(bundle != null && bundle.getInt("loadFragment") != 0)
-        {
+        if (bundle != null && bundle.getInt("loadFragment") != 0) {
             displayView(bundle.getInt("loadFragment"), bundle);
-        }
-        else
-        {
+        } else {
             displayView(0, null);
+        }
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            //selectItem(position);
         }
     }
 
